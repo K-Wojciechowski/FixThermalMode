ba3f16f27e72b974971b9f3d300ebebd1209010d
Krzysztof Wojciechowski <kw@krzysztofwojciechowski.pl>
2024-04-08 00:18:00 +0200

    Version 1.0.1.0

--- Build-Setup.ps1
+++ Build-Setup.ps1
@@ -3,6 +3,7 @@ $ErrorActionPreference = "Stop"
 Push-Location $PSScriptRoot
 
 try {
+  $Env:FTM_VERSION = (Get-Content -Raw version.txt).Trim()
   dotnet clean -v n .\FixThermalMode.Setup.sln
   dotnet build -v n .\FixThermalMode.Setup.sln -c:Release -p:Platform=x64
 } finally {
